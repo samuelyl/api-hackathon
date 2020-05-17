313807e49eede6184ca56a24c4a939b2762a7631
Sam Lee <samdagooner@gmail.com>
2020-05-17 16:44:19 -0700

    adjusted HTML CSS and JS to better utilize bootstrap plus changed loading modal and added new modal to display motivational gifs to results table

--- components/app.js
+++ components/app.js
@@ -1,9 +1,11 @@
 var baseURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/";
 var pageBody = $("body");
+var formContainer = document.getElementById("formContainer");
 var form = document.getElementById("searchForm");
 var button = document.getElementById("searchButton");
 var category = document.getElementById("businessSearch");
 var newLocation = document.getElementById("locationSearch");
+var modalContent = document.querySelector(".modal-content");
 var modal = document.querySelector(".modal");
 var errorText = document.querySelector(".error-text");
 form.addEventListener("submit", function(event){
@@ -30,17 +32,9 @@ function getSearch(){
     headers: {
       "Authorization": "Bearer " + yKey,
     },
-    success: function(data){
-      postData(data);
-    },
-    error: function (error) {
-      console.log(error);
-      // getErrorImage();
-      errorText.classList.remove("hide");
-    },
+    success: postData,
+    error: handleError,
   })
-  // console.log(data);
-  // console.log(myCategory, myLocation);
   hideLanding();
 }
 
@@ -58,40 +52,56 @@ function postData (data) {
     var col3 = document.createElement("td");
     col1.textContent = data.businesses[i].name;
     col2.textContent = data.businesses[i].display_phone;
-    col3.textContent = data.businesses[i].location.address1 + " " + data.businesses[i].location.city + ", " + data.businesses[i].location.country + " " + data.businesses[i].location.zip_code;
+    col3.textContent = data.businesses[i].location.address1 + " " + data.businesses[i].location.city + ", " + data.businesses[i].location.state + " " + data.businesses[i].location.country + " " + data.businesses[i].location.zip_code;
     row.append(col1, col2, col3);
     tBody.append(row);
-    resultTable.classList.remove("hidden");
+    resultTable.classList.remove("d-none");
+    if (!data.businesses[i].display_phone){
+      col2.textContent = "Not Listed";
+    }
+    if (!data.businesses[i].location.address1) {
+      col3.textContent = " " + data.businesses[i].location.city + ", " + data.businesses[i].location.state + " " + data.businesses[i].location.country + " " + data.businesses[i].location.zip_code;
+    }
   }
+  getImage();
 }
 
 function hideLanding (){
   if (event.target) {
-    form.classList.add("hidden")
+    form.classList.add("d-none")
     var landingPage = document.getElementById("landingPage");
     landingPage.classList.remove("background-image");
     landingPage.classList.add("new-background-image");
     var headerTitle = document.querySelector(".header-title");
     headerTitle.classList.add("new-header-title");
-    // console.log("it is hidden");
   }
+}
+
+function handleError (error) {
+  console.log(error);
+  errorText.textContent = "UH OH! Something Went Wrong!";
+  errorText.classList.remove("hide");
+  formContainer.classList.add("d-none");
 }
 
 // Image Search
 
 function getImage(){
   $.ajax({
-    url: "https://api.giphy.com/v1/gifs/random?api_key=" + gKey + "&tag=Motivation&rating=G",
+    url: "https://api.giphy.com/v1/gifs/random?api_key=" + gKey + "&tag=sports motivation&rating=G",
     method: "GET",
-    success: function(result){
-      console.log(result.data.image_original_url);
-      modal.style.backgroundImage = "url(" + result.data.image_original_url + ")";
-    },
+    success: postGif,
     error: function(error){
       console.log(error);
-      errorText.classList.remove("hide");
+      // errorText.textContent = "UH OH! Something Went Wrong!", error;
+      // errorText.classList.remove("hide");
     },
   })
+}
+
+function postGif(result){
+  console.log("Gif url", result.data.image_original_url);
+  modalContent.style.backgroundImage = "url(" + result.data.image_original_url + ")";
 }
 
 // function getErrorImage(){
@@ -114,9 +124,13 @@ function getImage(){
 $(document).on({
   ajaxStart: function () {
     pageBody.addClass("loading");
-    getImage();
+    getLoadingModal();
   },
   ajaxStop: function () {
     pageBody.removeClass("loading");
   },
 });
+
+function getLoadingModal (){
+  modal.style.backgroundImage = "url('https://media.giphy.com/media/11T6LuIxeHtJJu/giphy.gif')";
+}
